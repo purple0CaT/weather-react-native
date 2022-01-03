63d033c29fff75cb69d79ec301c0c67438431d3d
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-01-03 20:58:25 +0000

    testing

--- App.js
+++ App.js
@@ -1,23 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  SafeAreaView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  Text,
+  Image,
+  View,
+} from "react-native";
 
 export default function App() {
-  let x= 1
-  console.log("Console =>",x)
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Mis Karina, ne chvorijte, a to ja chvilujus 🥰</Text>
-      <Text> Hey</Text>
+      <Text>Touchable no feedback</Text>
+      <TouchableWithoutFeedback onPress={() => alert("Without feedback!")}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 200,
+            height: 150,
+          }}
+        />
+      </TouchableWithoutFeedback>
+      <Text>Touchable feedback</Text>
+      <TouchableOpacity onPress={() => alert("With feedback!")}>
+        <Image
+          source={{
+            uri: "https://picsum.photos/200/300",
+            width: 300,
+            height: 150,
+          }}
+        />
+      </TouchableOpacity>
+      <Text> Bottom</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ff3",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
