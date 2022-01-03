92f895e2fc3e04d9e2cfbd035150b5f1dc61a557
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-01-03 17:59:59 +0000

    created react native

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  let x= 1
+  console.log("Console =>",x)
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Mis Karina, ne chvorijte, a to ja chvilujus 🥰</Text>
+      <Text> Hey</Text>
       <StatusBar style="auto" />
     </View>
   );
