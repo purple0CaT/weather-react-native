67893825d30500ab2db7705da5d93a30962813a9
Istvan <17463906+purple0CaT@users.noreply.github.com>
2022-01-15 17:32:41 +0000

    update

--- style/mainStyle.js
+++ style/mainStyle.js
@@ -1,9 +1,10 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Platform } from "react-native";
 //
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? 20 : 0,
     // flexDirection: "row",
     // alignItems: "center",
     // justifyContent: "center",
